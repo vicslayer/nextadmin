9faab05c648677661ae2bf8bbd1dc0861dc609fa
vicslayer <probinsyanongitexpert@gmail.com>
2024-06-14 13:10:47 +0300

    new file:   app/dashboard/products/add/page.jsx 	modified:   app/dashboard/products/page.jsx 	modified:   app/ui/dashboard/dashboard.module.css 	modified:   app/ui/dashboard/pagination/pagination.jsx 	modified:   app/ui/dashboard/pagination/pagination.module.css 	new file:   app/ui/dashboard/products/addProduct/addProduct.module.css 	modified:   app/ui/dashboard/products/products.module.css

--- app/ui/dashboard/pagination/pagination.jsx
+++ app/ui/dashboard/pagination/pagination.jsx
@@ -1,6 +1,11 @@
+import styles from "./pagination.module.css";
+
 const Pagination=()=>{
     return(
-        <div>Pagination</div>
+        <div className={styles.container}>
+            <button className={styles.button} disabled>Previous</button>
+            <button className={styles.button}>Next</button>
+        </div>
     );
 };
 export default Pagination;
